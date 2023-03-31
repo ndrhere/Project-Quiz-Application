3312a91e4175e8997bc2e523e021486666f34869
ndrhere <ndx199@gmail.com>
2023-03-31 23:31:50 +0530

    Quiz Application

---
+++ javascript/coding.js
@@ -0,0 +1,422 @@
+const data = {
+    "music": [{
+        "id": 1,
+        "question": "Which of the following is not a type of music notation?",
+        "options": ["Standard notation", "Tab notation", "Morse code notation", "Graphics notation"],
+        "answer": "Morse code notation",
+        "category": "music"
+    },
+    {
+        "id": 2,
+        "question": "What is the most common time signature in classical music?",
+        "options": ["3/4", "4/4", "5/4", "6/8"],
+        "answer": "4/4",
+        "category": "music"
+    },
+    {
+        "id": 3,
+        "question": "Which of the following is not a type of instrument in a symphony orchestra?",
+        "options": ["Violin", "Piano", "Harp", "Theremin"],
+        "answer": "Theremin",
+        "category": "music"
+    },
+    {
+        "id": 4,
+        "question": "What is the most common key in pop music?",
+        "options": ["C Major", "G Major", "D Major", "A Major"],
+        "answer": "C Major",
+        "category": "music"
+    },
+    {
+        "id": 5,
+        "question": "Which of the following is not a type of chord?",
+        "options": ["Major", "Minor", "Diminished", "Flat"],
+        "answer": "Flat",
+        "category": "music"
+    },
+    {
+        "id": 6,
+        "question": "Which of the following is not a type of music genre?",
+        "options": ["Jazz", "Blues", "Rock", "Applesauce"],
+        "answer": "Applesauce",
+        "category": "music"
+    },
+    {
+        "id": 7,
+        "question": "Which of the following is not a type of music theory?",
+        "options": ["Harmony", "Counterpoint", "Form", "Cooking"],
+        "answer": "Cooking",
+        "category": "music"
+    },
+    {
+        "id": 8,
+        "question": "What is the most common tempo marking in classical music?",
+        "options": ["Allegro", "Andante", "Adagio", "Moderato"],
+        "answer": "Allegro",
+        "category": "music"
+    },
+    {
+        "id": 9,
+        "question": "Which of the following is not a type of musical form?",
+        "options": ["Sonata", "Symphony", "Concerto", "Spaghetti"],
+        "answer": "Spaghetti",
+        "category": "music"
+    },
+    {
+        "id": 10,
+        "question": "Which of the following is not a type of music notation software?",
+        "options": ["Sibelius", "Finale", "MuseScore", "Microsoft Word"],
+        "answer": "Microsoft Word",
+        "category": "music"
+    }
+    ],
+    "modernart": [{
+        "id": 11,
+        "question": "Which artist is known for coining the term 'Surrealism'?",
+        "options": ["Pablo Picasso", "Salvador Dali", "Vincent van Gogh", "Henri Matisse"],
+        "answer": "Salvador Dali",
+        "category": "modern-art"
+    },
+    {
+        "id": 12,
+        "question": "Which movement is associated with the use of abstract forms and shapes in art?",
+        "options": ["Impressionism", "Expressionism", "Futurism", "Cubism"],
+        "answer": "Cubism",
+        "category": "modern-art"
+    },
+    {
+        "id": 13,
+        "question": "Which artist is known for painting the work 'The Persistence of Memory'?",
+        "options": ["Pablo Picasso", "Salvador Dali", "Vincent van Gogh", "Henri Matisse"],
+        "answer": "Salvador Dali",
+        "category": "modern-art"
+    },
+    {
+        "id": 14,
+        "question": "Which artist is known for creating the painting 'The Scream'?",
+        "options": ["Vincent van Gogh", "Salvador Dali", "Edvard Munch", "Claude Monet"],
+        "answer": "Edvard Munch",
+        "category": "modern-art"
+    },
+    {
+        "id": 15,
+        "question": "What movement was associated with the use of bold, bright colors and thick brushstrokes?",
+        "options": ["Impressionism", "Expressionism", "Fauvism", "Cubism"],
+        "answer": "Fauvism",
+        "category": "modern-art"
+    },
+    {
+        "id": 16,
+        "question": "What movement was associated with the use of bright colors, simplified forms, and a focus on movement and speed",
+        "options": ["Impressionism", "Futurism", "Surrealism", "Abstract Expressionism"],
+        "answer": "Futurism",
+        "category": "modern-art"
+    },
+    {
+        "id": 17,
+        "question": "Which artist is known for creating the painting 'Water Lilies'?",
+        "options": ["Claude Monet", "Paul Cezanne", "Paul Gauguin", "Paul Klee"],
+        "answer": "Claude Monet",
+        "category": "modern-art"
+    },
+    {
+        "id": 18,
+        "question": "Which artist is known for creating the painting 'Les Demoiselles d'Avignon'?",
+        "options": ["Henri Matisse", "Vincent van Gogh", "Salvador Dali", "Pablo Picasso"],
+        "answer": "Pablo Picasso",
+        "category": "modern-art"
+    },
+    {
+        "id": 19,
+        "question": "Which artist is known for creating the painting 'Guernica'?",
+        "options": ["Claude Mone", "Paul Cezanne", "Pablo Picasso", "Vincent van Gogh"],
+        "answer": "Pablo Picasso",
+        "category": "modern-art"
+    },
+    {
+        "id": 20,
+        "question": "Which artist is known for creating the sculpture 'The Thinker'?",
+        "options": ["Auguste Rodin", "Alexander Calder", "Jean Arp", "Henry Moore"],
+        "answer": "Auguste Rodin",
+        "category": "modern-art"
+    }
+    ],
+    
+    "coding": [{
+        "id": 21,
+        "question": "What is the correct syntax for an if statement in Python?",
+        "options": ["if (condition):", "if condition", "if: condition", "if condition:"],
+        "answer": "if condition:",
+        "category": "coding"
+    },
+    {
+        "id": 22,
+        "question": "Which of the following is not a data type in JavaScript?",
+        "options": ["String", "Number", "Boolean", "ArrayList"],
+        "answer": "ArrayList",
+        "category": "coding"
+    },
+    {
+        "id": 23,
+        "question": "Which of the following is used to declare a variable in Java?",
+        "options": ["var", "let", "const", "int"],
+        "answer": "int",
+        "category": "coding"
+    },
+    {
+        "id": 24,
+        "question": "What is the correct syntax for a for loop in C#?",
+        "options": ["for i = 0 to 10", "for (i = 0; i <= 10; i++)", "for (int i = 0; i <= 10)", "for i in range(0, 10)"],
+        "answer": "for (i = 0; i <= 10; i++)",
+        "category": "coding"
+    },
+    {
+        "id": 25,
+        "question": "Which of the following is not a looping structure in PHP?",
+        "options": ["while", "for", "do-while", "foreach"],
+        "answer": "foreach",
+        "category": "coding"
+    },
+    {
+        "id": 26,
+        "question": "Which of the following is not a valid operator in C++?",
+        "options": ["+", "-", "*", "$"],
+        "answer": "$",
+        "category": "coding"
+    },
+    {
+        "id": 27,
+        "question": "In which programming language is 'print' used for displaying output?",
+        "options": ["Python", "JavaScript", "Java", "C++"],
+        "answer": "Python",
+        "category": "coding"
+    },
+    {
+        "id": 28,
+        "question": "What is the correct syntax for a function in Ruby?",
+        "options": ["function name()", "def name", "function name", "def name()"],
+        "answer": "def name()",
+        "category": "coding"
+    },
+    {
+        "id": 29,
+        "question": "Which of the following is not a type of variable in Swift?",
+        "options": ["Int", "String", "Double", "Object"],
+        "answer": "Object",
+        "category": "coding"
+    },
+    {
+        "id": 30,
+        "question": "In which programming language is '#' used for commenting?",
+        "options": ["Python", "JavaScript", "Java", "C++"],
+        "answer": "C++",
+        "category": "coding"
+    }
+    ]
+    }
+    console.log(data);
+    
+    let index= 0;
+    
+    let startQuiz= ()=>{
+    
+       document.getElementById('questiondata').innerHTML = ` ${index+1}. ${data.coding[index].question}`
+       document.getElementById('option1').nextElementSibling.innerHTML= data.coding[index].options[0]
+       document.getElementById('option2').nextElementSibling.innerHTML= data.coding[index].options[1]
+       document.getElementById('option3').nextElementSibling.innerHTML= data.coding[index].options[2]
+       document.getElementById('option4').nextElementSibling.innerHTML= data.coding[index].options[3]
+       document.getElementsByClassName('container1')[0].style.display= 'none';
+    
+        let nextButton = document.getElementById('nextbtn')
+        nextButton.disabled= true;
+    
+        let choosenOption = document.getElementsByTagName('input');
+        for(let i = 0; i<choosenOption.length; i++){
+           choosenOption[i].addEventListener('change', function(){
+            nextButton.disabled = false;
+           })
+        }
+         nextButton.disabled = true;
+    
+    }
+    
+    
+    
+    
+    let choosenOption = document.getElementsByTagName('input')
+    console.log(choosenOption)
+    //let arrayOption = Array.from(choosenOption)//
+    
+    
+    //document.getElementsByTagName gives and HTML element so we can't use forEach loop it it as forEach loop works in an array to make it an array we have to use Array.from which will give the array of all the input elements
+    
+    
+    
+    
+    let right = 0;
+    let wrong= 0;
+    let total = data.coding.length;
+    
+    let next = ()=>{
+        // let optionSelected = document.querySelector('input[name="option1"]:checked');
+        
+        let rightAns = data.coding[index].answer
+        let givenAns = markedAns();
+        // let givenAns = optionSelected.nextElementSibling.innerText
+        // console.log(givenAns)
+    
+    //Here we can't use optionSelecetd.nextElementSibling.innerText as the givenAns because how can we give define option selecetd prior to the occassion when user checks his or her answer //
+    
+       if(index < data.coding.length-1 ){
+    
+       
+    
+        index++;
+    
+        let requirement = document.querySelector('input[name="option1"]:checked').id
+        
+        localStorage.setItem(`The userAnswer for Q${index} is`, requirement);
+    
+        //We have to write local storage code aboove the code where we are making optionSelected.checked value as false as by not doing this it will make the checked false prior to the requirement where we want it to be stored first and then has to be unchecked//
+       
+        
+        let optionSelected = document.querySelector('input[name="option1"]:checked');
+        console.log(optionSelected)
+        if(optionSelected){
+            
+            optionSelected.checked= false;
+        
+        }
+    
+    
+    
+        startQuiz();
+        //Here we can't use while loop because while loop gives the value all at a once if the condition is true but what we want is that to get the condition checked once we click on the button
+        
+       }
+    
+       if(givenAns==rightAns){
+        right++;
+    
+        
+       }
+       else{
+        wrong++;
+       
+       }
+    console.log(right, wrong)
+       
+       
+       
+    
+       if (index===data.coding.length-1){
+        console.log("Index is equal to data.coding.length");
+        
+        document.getElementsByClassName('btn')[1].style.display= 'none';
+        document.getElementsByClassName('btn')[2].style.display='inline-block';
+       }
+       
+       
+    //    let userData = localStorage.setItem(`UserAnswer for Q.${index+1}, ${optionSelected.id}`)
+       
+        
+    }
+    
+    
+    
+    
+        let submit = ()=>{
+            let index = data.coding.length-1;
+            let rightAnswer = data.coding[index].answer
+            console.log(rightAnswer);
+            let givenAnswer = markedAns();
+            if(rightAnswer==givenAnswer){
+                right++;
+            }else{
+                wrong++;
+            }
+    
+    
+            index++;
+            let requirement = document.querySelector('input[name="option1"]:checked').id
+    
+        //As requirement is already a string we don't have a need to used JSON.stringify thinking that the local storage takes string as the value and key and stringify converts a number, boolean, object to a string. As it is aleady a string we don't have to use stringify here//
+    
+            localStorage.setItem(`The userAnswer for Q${index} is`, requirement);
+    
+            scoreCard()
+        
+    }
+    
+    
+    
+    
+    let scoreCard = ()=>{
+        let rightString= right.toString();
+    
+    document.getElementsByClassName('container')[0].style.display= 'none';
+    document.getElementsByClassName('btn')[0].style.display='none';
+    document.getElementsByClassName('btn')[2].style.display= 'none';
+    document.getElementsByClassName('container1')[0].style.display= 'inline-block';
+    
+    document.getElementsByClassName('container1')[0].innerHTML=
+    `<h2>Thank you for the Submission. Your Result for the quiz is : </h2>
+    <h4> Total Questions = ${total}</h4>
+    <h4> Right Answers = ${right}</h4>
+    <h4> Wrong Answers = ${total-right}</h4>
+    <h4> Percentage Marks = ${right/total *100}</h4>
+    `
+    
+    localStorage.setItem(`The Total Score of the User is`, `${rightString}`)
+    
+    
+    }
+    
+    
+    
+    // let markedAns=()=>{
+    //     let userAns;
+    //     arrayOption.forEach((input)=>{
+        
+    //     if(input.checked){
+    //          userAns= input.nextElementSibling.innerText
+    //     }
+    // })
+    // return userAns;
+    
+    // }
+    
+    let markedAns = ()=>{
+        let userAns;
+        for(let i = 0; i<choosenOption.length; i++)
+        if(choosenOption[i].checked){
+            userAns=choosenOption[i].nextElementSibling.innerText
+        }
+        return userAns;
+    }
+    
+    //If we want to go with the HTML Collection itself we have to use for loop like above//
+    
+    let previous = ()=>{
+        if(index>0){
+            index--;
+         
+            startQuiz()
+            let nextButton = document.getElementById('nextbtn');
+            nextButton.disabled = false;
+      let retrieve= localStorage.getItem(`The userAnswer for Q${index} is`);
+      console.log(typeof retrieve)
+      
+      
+      let choosenOption = document.getElementsByTagName('input')
+      for(let i = 0; i<choosenOption.length; i++){
+      if(choosenOption[i].id===retrieve){
+          choosenOption[i].checked= true;
+      }
+    }
+            
+        }
+    }
+    
+    
+    startQuiz()
